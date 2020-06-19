209e873a10dd1d3e7b2ecc949a91f1dcc591c07b
winifredf <winifred.dykes@hotmail.com>
2020-06-18 22:55:05 -0400

    created users migration

--- db/migrate/20200619024937_create_skills.rb
+++ db/migrate/20200619024937_create_skills.rb
@@ -1,4 +1,10 @@
 class CreateSkills < ActiveRecord::Migration
   def change
+    create_table :skills do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
